cee5d824999bcd16909f660ffd1c710476be9c52
Michael Albrecht <albrecht@fortiss.org>
2024-01-18 14:13:29 +0100

    put def overlaps in a new function + added fuction to create quadtree to  search

--- forwardpass/get_land_usage.py
+++ forwardpass/get_land_usage.py
@@ -24,6 +24,69 @@ def find_usetype_point(shapefile, x,y):
         
     raise Exception(f"Point ({x},{y}) not in shapefile.")
 
+def Overlaps(bbox1, bbox2):
+    """
+    Returns True if bbox1 and bbox2 overlap.
+    """
+    if (bbox1[0] <= bbox2[0] <= bbox1[2] and bbox1[1] <= bbox2[1] <= bbox1[3])\
+        or (bbox1[0] <= bbox2[2] <= bbox1[2] and bbox1[1] <= bbox2[3] <= bbox1[3])\
+            or (bbox1[0] <= bbox2[0] <= bbox1[2] and bbox1[1] <= bbox2[3] <= bbox1[3])\
+                or (bbox1[0] <= bbox2[2] <= bbox1[2] and bbox1[1] <= bbox2[1] <= bbox1[3]):
+        return True
+    else:
+        return False
+    
+def create_search_tree(shapefile):
+    """
+    Creates a search tree for the shapefile.
+    """
+    splits = 4
+    tree = {}
+    threshold_x = (shapefile.bbox [2] - shapefile.bbox [0]) / (splits/2) + shapefile.bbox [0]
+    threshold_y = (shapefile.bbox [3] - shapefile.bbox [1]) / (splits/2) + shapefile.bbox [1]
+
+    #create tree
+    tree[shapefile.bbox[0],shapefile.bbox[1]] = {}
+    tree[shapefile.bbox[0],threshold_y] = {}
+    tree[threshold_x,shapefile.bbox[1]] = {}
+    tree[threshold_x,threshold_y] = {}
+
+    #add shapes to tree
+    for shape in shapefile.shapeRecords():
+        #bottom left
+        if shape.shape.bbox[0] <= threshold_x:
+            if shape.shape.bbox[1] <= threshold_y:
+                #q3
+                tree[shapefile.bbox[0],shapefile.bbox[1]].update({tuple(shape.shape.bbox):shape.record.nutzart})
+            else:
+                #q2
+                tree[shapefile.bbox[0],threshold_y].update({tuple(shape.shape.bbox):shape.record.nutzart})
+        else:
+            if shape.shape.bbox[1] <= threshold_y:
+                #q4
+                tree[threshold_x,shapefile.bbox[1]].update({tuple(shape.shape.bbox):shape.record.nutzart})
+            else:
+                #q1
+                tree[threshold_x,threshold_y].update({tuple(shape.shape.bbox):shape.record.nutzart})
+        #top right
+        if shape.shape.bbox[2] <= threshold_x:
+            if shape.shape.bbox[3] <= threshold_y:
+                #only in q3
+                pass
+            else:
+                #q2
+                tree[shapefile.bbox[0],threshold_y].update({tuple(shape.shape.bbox):shape.record.nutzart})
+        else:
+            if shape.shape.bbox[3] <= threshold_y:
+                #q4
+                tree[threshold_x,shapefile.bbox[1]].update({tuple(shape.shape.bbox):shape.record.nutzart})
+            else:
+                #q1
+                tree[threshold_x,threshold_y].update({tuple(shape.shape.bbox):shape.record.nutzart})
+    return tree
+        
+
+
 
 def find_usetype_inside_bb(shapefile, bbox, use_list):
     """
@@ -32,8 +95,7 @@ def find_usetype_inside_bb(shapefile, bbox, use_list):
     use = False
     found = False
     for shape in shapefile.shapeRecords():
-        if (bbox[0] <= shape.shape.bbox[0] <= bbox[2] and bbox[1] <= shape.shape.bbox[1] <= bbox[3])\
-            or (bbox[0] <= shape.shape.bbox[2] <= bbox[2] and bbox[1] <= shape.shape.bbox[3] <= bbox[3]):
+        if Overlaps(bbox, shape.shape.bbox):
             found = True
             if check_if_usable(shape.record.nutzart, use_list):
                 use = True
